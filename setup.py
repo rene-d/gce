edc848f6c9bfcfb6929380ee0951550ab44a9b40
René Devichi <rene.devichi@gmail.com>
2019-12-25 20:27:43 +0100

    update info

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="gce",
-    version="1.0.0",
+    version="1.0.1",
     description="Script Python pour interroger un module Teleinfo de GCE Electronics",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,12 +20,15 @@ setup(
     author_email="rene.github@gmail.com",
     classifiers=[
         "License :: Public Domain",
+        "Operating System :: OS Independent",
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
-    keywords="development automation",
+    keywords="teleinfo teleinformation gce",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     python_requires=">=3.5, <4",
